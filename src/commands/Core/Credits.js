8734d91d07f742670c1da713c477460cb58cc703
FrostyAwo <aawokiengamess@gmail.com>
2026-06-05 12:31:02 -0500

    Update Credits.js with new roles and formatting

---
+++ src/commands/Core/Credits.js
@@ -0,0 +1,57 @@
+import { SlashCommandBuilder } from 'discord.js';
+import { createEmbed } from '../../utils/embeds.js';
+import { logger } from '../../utils/logger.js';
+import { InteractionHelper } from '../../utils/interactionHelper.js';
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName('credits')
+    .setDescription('View bot credits'),
+
+  async execute(interaction) {
+    try {
+      await InteractionHelper.safeDefer(interaction);
+
+      await InteractionHelper.safeEditReply(interaction, {
+        embeds: [
+          createEmbed({
+            title: '📜 Credits',
+            description: `
+## SnowyAwo Moderation
+
+👑 **Owner & Developer**
+SnowyAwo
+
+👨‍💻 **CoOwner**
+Ducky 
+
+🎨 **Design**
+SnowyAwo
+
+🧪 **Server Staff**
+SnowyAwo
+Ducky
+Oreo
+1dgkids2012
+Pekky
+
+━━━━━━━━━━━━━━━━━━
+
+© 2026 SnowyAwo
+All Rights Reserved.
+`,
+            thumbnail: interaction.client.user.displayAvatarURL({
+              forceStatic: false,
+              size: 1024,
+            }),
+            footer: {
+              text: 'SnowyAwo Moderation • Credits',
+            },
+          }),
+        ],
+      });
+    } catch (error) {
+      logger.error('Credits command error:', error);
+    }
+  },
+}; 
