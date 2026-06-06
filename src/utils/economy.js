b6e21af216009938bf09cdf21e521dc59856318c
FrostyAwo <aawokiengamess@gmail.com>
2026-06-05 20:33:04 -0500

    Increase base bank capacity to a larger value

--- src/utils/economy.js
+++ src/utils/economy.js
@@ -6,7 +6,7 @@ import { validateDiscordId, validateNumber } from './validation.js';
 import { DEFAULT_ECONOMY_DATA } from './constants.js';
 
 const ECONOMY_CONFIG = BotConfig.economy || {};
-const BASE_BANK_CAPACITY = ECONOMY_CONFIG.baseBankCapacity || 10000;
+const BASE_BANK_CAPACITY = ECONOMY_CONFIG.baseBankCapacity || 100000000000000000000000000000000000000000000000000000000000000;
 const BANK_CAPACITY_PER_LEVEL = ECONOMY_CONFIG.bankCapacityPerLevel || 5000;
 const DAILY_AMOUNT = ECONOMY_CONFIG.dailyAmount || 100;
 const WORK_MIN = ECONOMY_CONFIG.workMin || 10;
